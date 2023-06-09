9f215d47eeff6b69876543faf77246e2d223e52f
python-game <n.marforio@gmail.com>
2023-06-09 13:31:31 +0200

    just started with an input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SearchInput from "./Components/SearchBar";
+import { useState } from "react";
 
-function App() {
+const App: React.FC = () => {
+  const [ingridient, setingridient] = useState<string>("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipe Ideas</h1>
+      <SearchInput />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/SearchBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchInput = () => {
+  return (
+    <form>
+      <input type="input" placeholder="ingridients"></input>
+      <button type="submit">TASTY!</button>
+    </form>
+  );
+};
+
+export default SearchInput;
